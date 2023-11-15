ebc8952767d535db96d0e24adb32cc79cc26102f
attepower <atte@atte.pl>
2023-11-15 14:31:07 +0100

    after class

--- 03-ClassStructure/MyArray.java
+++ 03-ClassStructure/MyArray.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+
 public class MyArray {
+    int[] array;
     public static int even(int[] array) {
         int num=0; 
         for (int i = 0; i < array.length; i++) {
@@ -17,15 +20,69 @@ public class MyArray {
         }
         return num; 
     }
-    public static int[] reverse(int[] array) {
-        int[] reversedArray = new int[array.length];
-    
-        for (int i = 0; i < array.length; i++) {
-            reversedArray[i]=0;
+    public static String reverse(int[] array){
+        int len = array.length;
+        int[] reversedArray= new int[len];
+        for (int i=0 ; i < len;i++){
+            reversedArray[i]=array[len-1-i];
         }
-        
-        return reversedArray;
+        return Arrays.toString(reversedArray);
     }
-    
+    public static boolean compare(int[] array1, int[] array2){
+        boolean wartosc = false;
+        if (array1.length==array2.length){
+            wartosc = false;
+            for (int i = 0; i<array1.length;i++){
+                if (array1[i]==array2[i]){
+                    wartosc= true;
+                } else{
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+        return wartosc;
+    }
+    public static boolean different(int[] array1, int[] array2){
+        
+    }
+    public static boolean exists(int number, int[] array){
+        int len = array.length;
+        int wartosc = 0;
+        for (int i = 0;i<len;i++){
+            if (array[i]==number){
+                wartosc++;
+            }else{
+                wartosc+=0;
+            }
+        }
+        return len==wartosc;
+    }
+    public static int secondMax(int[] array){
+        int firstMax= -2137;
+        int secondMax= -2137;
+        int len = array.length;
+        for (int i = 0;i<len;i++){
+            if (array[i]>firstMax){
+                firstMax= array[i];
+            }
+        }
+        for (int i = 0;i<len;i++){
+            if (array[i]>secondMax && secondMax<firstMax){
+                secondMax= array[i];
+            }
+        }
+        return secondMax;
+    }
+    public static int lastColumn(int[][] array1){
+        
+    }
+    public static int[][] swap(int[][] array1){
+        
+    }
+    public static int[] twoToOne(int[][] array){
+
+    }
     
 }
--- 03-ClassStructure/MyArrayTest.java
+++ 03-ClassStructure/MyArrayTest.java
@@ -1,8 +1,10 @@
 public class MyArrayTest {
     public static void main(String[] args) {
         int[] mojaTablica = {-1,-3,1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,14,16};
+        int[] mojaTablica2 = {-1,-3,1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,14,16};
         System.out.println(MyArray.even(mojaTablica));
         System.out.println(MyArray.positiveOdd(mojaTablica));
         System.out.println(MyArray.reverse(mojaTablica));
+        System.out.println(MyArray.compare(mojaTablica,mojaTablica2));
     }
 }
